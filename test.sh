631a1281b5e52067ad52310494bb691ad58894c2
anuj1102 <anuj1102@gmail.com>
2020-03-15 18:54:16 +0000

    work

--- test.sh
+++ test.sh
@@ -10,3 +10,13 @@ rm -rf ~/.local/bin/abduco
 echo "Rerunning install script to see if all programs are installed"
 sleep 1
 ./install.sh
+
+function check_dep() {
+   command -v $1 >/dev/null 2>&1 || {
+     echo >&2 "Test failed! $1 not found. $2"; exit 1;
+  }
+}
+
+check_dep zsh
+check_dep abduco
+check_dep nvim
