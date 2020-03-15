110218de63a5c2288f26506bda641503a5983703
anuj1102 <anuj1102@gmail.com>
2020-03-15 19:20:08 +0000

    Fixed tests, added abduco to zplug

--- test.sh
+++ test.sh
@@ -1,21 +1,22 @@
-echo "Remove zplug and zsh"
-rm -rf ~/.zplug ~/.zshrc
+#!/bin/bash
 
-echo "Remove neovim"
-rm -rf ~/.config/nvim/init.vim ~/.local/share/nvim ~/.local/bin/nvim
+ echo "Remove zplug and zsh"
+ rm -rf ~/.zplug ~/.zshrc
 
-echo "Remove abduco"
-rm -rf ~/.local/bin/abduco
+ echo "Remove neovim"
+ rm -rf ~/.config/nvim/init.vim ~/.local/share/nvim ~/.local/bin/nvim
 
-echo "Rerunning install script to see if all programs are installed"
-sleep 1
-./install.sh
+ echo "Remove abduco"
+ rm -rf ~/.local/bin/abduco
+
+ echo "Rerunning install script to see if all programs are installed"
+ sleep 1
+ ./install.sh
 
 function check_dep() {
-   command -v $1 >/dev/null 2>&1 || {
-     echo >&2 "Test failed! $1 not found. $2"; exit 1;
-  }
+	command -v $1 >/dev/null 2>&1 || { echo >&2 "Test Failed! $1 not found. $2"; exit 1; }
 }
+
 
 check_dep zsh
 check_dep abduco
