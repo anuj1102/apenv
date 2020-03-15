47b5a483af790e829e1ac347a304bd0101a413f5
anuj1102 <anuj1102@gmail.com>
2020-03-15 11:28:29 +0000

    Finished automation, added tests, consider using zplug to install packages in the future

---
+++ test.sh
@@ -0,0 +1,10 @@
+echo "Remove zplug and zsh"
+rm -rf ~/.zplug ~/.zshrc
+
+echo "Remove neovim"
+rm -rf ~/.config/nvim/init.vim ~/.local/share/nvim ~/.local/bin/nvim
+
+echo "Remove abduco"
+rm -rf ~/.local/bin/abduco
+
+echo "Verify install.sh script installs these programs"
