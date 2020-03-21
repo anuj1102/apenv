51eafffc6bed571bcfc8f15c440338c66cb8bf08
Anuj Patel <anujp@meraki.com>
2020-03-20 23:10:35 -0700

    Added ruby env and solargraph

---
+++ ruby/install.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# Run these commands after setting rbenv shell 2.5.1
+rbenv shell 2.5.1
+ruby --version
+gem install bundler
+gem install solargraph
+gem install rubocop
---
+++ ruby/run.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+# Run these commands after setting rbenv shell 2.5.1
+rbenv shell 2.5.1
+solargraph/bin/socket
