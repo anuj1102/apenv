018ff1c38458bf19f251c991bebbdf712f3804ab
anuj1102 <anuj1102@gmail.com>
2020-03-21 12:23:48 +0000

    got basic neovim install working but cant autoload plugins right now

--- test.sh
+++ test.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
-
- echo "Remove zplug and zsh"
- rm -rf ~/.zplug ~/.zshrc ~/.p10k.zsh
-
- echo "Remove neovim"
- rm -rf ~/.config/nvim/init.vim ~/.local/share/nvim ~/.local/bin/nvim
-
- echo "Remove abduco"
- rm -rf ~/.local/bin/abduco
-
- echo "Remove nvr"
- rm -rf ~/.local/bin/nvr
-
- echo "Rerunning install script to see if all programs are installed"
- sleep 1
- ./install.sh
+#
+# Checks that all base programs exist
 
 function check_dep() {
 	command -v $1 >/dev/null 2>&1 || { echo >&2 "Test Failed! $1 not found. $2"; exit 1; }
