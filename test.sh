f39f4869d09eb52983618348dfa9934d636df9b4
anuj1102 <anuj1102@gmail.com>
2020-03-21 12:33:39 +0000

    Done refactor, got things working and wrote test.sh to demo full lifecycle

--- test.sh
+++ test.sh
@@ -2,6 +2,9 @@
 #
 # Checks that all base programs exist
 
+./install-profile.sh clean
+./install-profile.sh all
+
 function check_dep() {
 	command -v $1 >/dev/null 2>&1 || { echo >&2 "Test Failed! $1 not found. $2"; exit 1; }
 }
@@ -11,3 +14,5 @@ check_dep zsh
 check_dep abduco
 check_dep nvim
 check_dep nvr
+
+echo "Tests passed! All programs found!"
